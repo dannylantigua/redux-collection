5bcc3e5e2840a895cef438d9a8455f178efc9e2f
Danny Lantigua <dannylantigua@Dannys-MBP.hsd1.fl.comcast.net>
2020-01-26 14:08:10 -0500

    Finished SuperSquat react-redux demo

--- supersquad/src/index.js
+++ supersquad/src/index.js
@@ -6,9 +6,15 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
+import { addCharacterById } from './actions';
 
 const store = createStore(rootReducer);
+// Shows the store data / state
 console.log('store.getState()', store.getState());
+// Shows every time the store gets updated
+store.subscribe(() => console.log('store', store.getState()));
+// Gets the record on memory from the list (removes it and store it here)
+store.dispatch(addCharacterById(2));
 
 ReactDOM.render(
     <Provider store={store}>
--- supersquad/src/reducers/index.js
+++ supersquad/src/reducers/index.js
@@ -1,10 +1,11 @@
-import characters_json from '../data/characters.json';
+import { combineReducers } from 'redux';
 
-function characters(state = characters_json, action){
-    switch(action.type) {
-        default:
-            return state;
-    }
-}
+import characters from './characters-reducer';
+import heroes from './heroes-reducer';
 
-export default characters;
+const rootReducer = combineReducers({
+    characters,
+    heroes
+})
+
+export default rootReducer;
