b3f5bf268399da21c33d2c79214fd40e26e01fb5
Danny Lantigua <lantiguagrullon@gmail.com>
2020-02-01 14:30:29 -0500

    Added recipepuppy API call'

--- recipe-finder/src/components/SearchRecipes.js
+++ recipe-finder/src/components/SearchRecipes.js
@@ -2,22 +2,46 @@ import React, { Component } from 'react';
 import { Form, FormGroup, FormControl, Button } from 'react-bootstrap';
 
 class SearchRecipes extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            ingredients: '',
+            dish: ''
+        }
+    }
+
+    search(){
+        let { ingredients, dish } = this.state;
+        const url = `http://www.recipepuppy.com/api/?${ingredients}&q=${dish}`;
+        console.log('state', this.state, 'url', url);
+
+        fetch(url, {
+            method: 'GET'
+        }).then(response => response.json())
+            .then(json => console.log('recipes', json))
+    }
+
     render() {
         return (
             <Form inline>
                 <FormGroup>
                     <Form.Label>Ingredients</Form.Label>
                     {' '}
-                    <FormControl type="text" placeholder="Pesto, Tomatoes" />
+                    <FormControl 
+                    type="text" placeholder="Pesto, Tomatoes" 
+                    onChange={event => this.setState({ ingredients: event.target.value })}/>
                 </FormGroup>
                 {' '}
                 <FormGroup>
                     <Form.Label>Dish</Form.Label>
                     {' '}
-                    <FormControl type="text" placeholder="Pasta" />
+                    <FormControl 
+                    type="text" placeholder="Pasta" 
+                    onChange={event => this.setState({ dish: event.target.value })}/>
                 </FormGroup>
                 {' '}
-                <Button>Submit</Button>
+                <Button onClick={() => this.search()}>Submit</Button>
             </Form>
         )
     }
